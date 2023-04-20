af1e296808e3e27c5b21432caf00f3527779eb92
Dustin Swecker <dustin.swecker@gmail.com>
2023-04-20 11:22:20 -0400

    got shortboxed working to provide new comic book titles to read

--- script.mjs
+++ script.mjs
@@ -3,56 +3,60 @@ var searchBar = document.querySelector('#searchbar');
 var navUlEl = document.querySelector('#navul');
 var localStorageArray;
 var newReleases = document.querySelector('#release');
+var workInput = document.querySelector('#workinput');
 var charImg = $('.char-img')
 import { availableTags } from "./longstring.mjs";
 
-var comicVineApiKey = "035a63a70b2603f70c844e241cc5b85ba8cdd3db";
+//api urls and keys
 var shortBoxedUrl = "api.shortboxed.com"
-var comicVineUrl = "https://comicvine.gamespot.com/api"
-
 var key1 = "3299094467007947"
 var superUrl = "https://superheroapi.com/api/" + key1
 
 //Search button funtionality. Works but needs to be further developed for API use
-$('.img-size').on('click', function(event) {
-  console.log("It works")
-  event.preventDefault()
-  var event = $(this).prev().val()
-  console.log(event)
+// $('.img-size').on('click', function(event) {
+//   console.log("It works")
+//   event.preventDefault()
+//   var event = $(this).prev().val()
+//   console.log(event)
 
-  //Fetch the character's image
-  //fetch().then().then() must be used for each use (i.e. bio, work, etc.)
-  var requestUrl = superUrl;
-  fetch('https://cors-anywhere-jung.herokuapp.com/'+requestUrl, {
-  method: 'GET',
-  credentials: 'same-origin',
-  redirect: 'follow'
-  })
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    //Not targetting array for searched superhero. Shows everything not as Object or Array(x)
-  getSuperheroApi('/search/' + event)
-})
-})
+//   //Fetch the character's image
+//   //fetch().then().then() must be used for each use (i.e. bio, work, etc.)
+//   var requestUrl = superUrl;
+//   fetch('https://cors-anywhere-jung.herokuapp.com/'+requestUrl, {
+//   method: 'GET',
+//   credentials: 'same-origin',
+//   redirect: 'follow'
+//   })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     //Not targetting array for searched superhero. Shows everything not as Object or Array(x)
+//   getSuperheroApi('/search/' + event)
+// })
+// })
 
-//generic pull for comicvine api, use REQUESTED ELEMENT as paramater to specify which element within the shortboxed api to pull
-var getShortBoxedApi = function(requestedElement) {
-    var requestUrl = shortBoxedUrl + requestedElement;
-    console.log(requestUrl);
-    fetch('https://cors-anywhere-jung.herokuapp.com/'+requestUrl, {
+//generic pull for shortboxed api, use REQUESTED ELEMENT as paramater to specify which element within the shortboxed api to pull
+var getShortBoxedApi = function() {
+
+    var requestUrl2 = shortBoxedUrl + "/comics/v1/new";
+    
+    fetch('https://cors-anywhere-jung.herokuapp.com/'+requestUrl2, {
     method: 'GET',
     credentials: 'same-origin',
     redirect: 'follow'
     })
     .then(function (response) {
-      console.log(response);
+      
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      return(data);
+      
+      for (var i = 0; i<3; i++) {
+      var newToRead = document.createElement('p');
+      newToRead.textContent = "Title: " + data.comics[Math.floor(Math.random()*184)].title;
+      newReleases.appendChild(newToRead);
+      }
     
       })
 };
@@ -70,24 +74,16 @@ var getSuperheroApi = function(requestedElement) {
   .then(function (data) {
     
     console.log(data);
+    
     return data;
+
     }) 
 };
 
-// var getNewReleases = function() {
-//   var apiCall = getShortBoxedApi("/comics/v1/new");
-//   console.log(apiCall);
-//   var publisherInfo = document.createElement('p');
-//   publisherInfo.textContent = apiCall;
-//   newReleases.append(publisherInfo);
+// document.ready to place functions we don't want to use until the page has loaded, commented out for now
+$(document).ready(function() {
   
-
-// }
-
-//document.ready to place functions we don't want to use until the page has loaded, commented out for now
-// $(document).ready(function() {
-
-// });
+});
 
 //creating function to store data from the event listener.
 var inputToSearch = function (event) {
@@ -115,12 +111,19 @@ $(function() {
     });
   } );
 
-var checkingstuff = getShortBoxedApi("/comics/v1/new");
-console.log(checkingstuff);
+
 // getNewReleases();
+
 getSuperheroApi('/search/ironman/');
 
+
 //event listener for the super search bar
-searchBar.addEventListener('submit', inputToSearch);
+searchBar.addEventListener('submit', function(event){
+  event.preventDefault();
+  // userInput = searchInput.value.trim();
+  getShortBoxedApi();
+  
+    
+});
 
 
