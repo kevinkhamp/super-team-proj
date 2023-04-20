2b21cd0d0efc753b126fa4402349c422d5904d1c
EBosen <ethbosen@gmail.com>
2023-04-20 16:26:37 -0400

    Biography, Connections, Work all in working order

--- script.mjs
+++ script.mjs
@@ -4,8 +4,16 @@ var navUlEl = document.querySelector('#navul');
 var localStorageArray;
 var newReleases = document.querySelector('#release');
 var workInput = document.querySelector('#workinput');
-var biographyInput = document.querySelector('#biographyinput');
-var connectionsInput = document.querySelector('#connectionsinput');
+var workInput2 = document.querySelector('#workinput2');
+var biographyInput1 = document.querySelector('#biographyinput1');
+var biographyInput2 = document.querySelector('#biographyinput2');
+var biographyInput3 = document.querySelector('#biographyinput3');
+var biographyInput4 = document.querySelector('#biographyinput4');
+var biographyInput5 = document.querySelector('#biographyinput5');
+var biographyInput6 = document.querySelector('#biographyinput6');
+var biographyInput7 = document.querySelector('#biographyinput7');
+var connectionsInput1 = document.querySelector('#connectionsinput1');
+var connectionsInput2 = document.querySelector('#connectionsinput2'); 
 var charImg = $('.char-img')
 import { availableTags } from "./longstring.mjs";
 var favBtn = document.querySelector('#favbutton');
@@ -56,19 +64,22 @@ var getSuperheroApi = function(requestedElement) {
   })
   .then(function (data) {
     
-  console.log(data.results);
+  console.log(data.results[0].work);
 
   //Ethan's Bio
-  biographyInput.textContent = data.results[0].biography.aliases + 
-  // biographyInput.textContent = data.results[0].biography.allignment;
-  // biographyInput.textContent = data.results[0].biography.alter-egos;
-  // biographyInput.textContent = data.results[0].biography.first-appearance;
-  // biographyInput.textContent = data.results[0].biography.full-name;
-  // biographyInput.textContent = data.results[0].biography.place-of-birth;
-  biographyInput.textContent = data.results[0].biography.publisher;
-  
-  console.log(biographyInput)
-  console.log(data.results[0].biography)
+  biographyInput1.textContent = "Aliases: " +data.results[0].biography['aliases'] 
+  biographyInput2.textContent = "Alignment: " +data.results[0].biography['alignment']
+  biographyInput3.textContent = "Alter Egos: " +data.results[0].biography['alter-egos']
+  biographyInput4.textContent = "First Appearance: " +data.results[0].biography['first-appearance']
+  biographyInput5.textContent = "Full Name: " +data.results[0].biography['full-name']
+  biographyInput6.textContent = "Place of Birth: " +data.results[0].biography['place-of-birth']
+  biographyInput7.textContent = "Publisher: " +data.results[0].biography['publisher']
+
+  connectionsInput1.textContent = "Groups and Affiliations: " +data.results[0].connections['group-affiliation']
+  connectionsInput2.textContent = "Relatives: " +data.results[0].connections['relatives']
+
+  workInput.textContent = "Occupation: " +data.results[0].work['occupation']
+  workInput2.textContent = "Base: " +data.results[0].work['base']
 
     }) 
 };
